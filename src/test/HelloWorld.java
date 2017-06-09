5b4b84548266e7917d261cb901672c515e7def78
tongleiketong <625083654@qq.com>
2017-06-09 17:40:52 +0800

    这是真的

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * Created by 62508 on 2017/6/9.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+
+        System.out.println("Hellow");
+    }
+}
